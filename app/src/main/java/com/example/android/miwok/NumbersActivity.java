8dca7a62ba1ab0369b0e38b2cdc7151fa8cbf00c
Imre Julac <ijulac@yahoo.com>
2018-03-13 14:09:54 +0100

    Add TextView Layouts based on the ArrayList (without loops)

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -27,16 +29,19 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        Log.i("NumbersActivity", "Word at index 0: " + words.get(0));
-        Log.i("NumbersActivity", "Word at index 1: " + words.get(1));
-        Log.i("NumbersActivity", "Word at index 2: " + words.get(2));
-        Log.i("NumbersActivity", "Word at index 3: " + words.get(3));
-        Log.i("NumbersActivity", "Word at index 4: " + words.get(4));
-        Log.i("NumbersActivity", "Word at index 5: " + words.get(5));
-        Log.i("NumbersActivity", "Word at index 6: " + words.get(6));
-        Log.i("NumbersActivity", "Word at index 7: " + words.get(7));
-        Log.i("NumbersActivity", "Word at index 8: " + words.get(8));
-        Log.i("NumbersActivity", "Word at index 9: " + words.get(9));
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        TextView wordView = new TextView(this);
+        wordView.setText(words.get(0));
+        rootView.addView(wordView);
+
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(words.get(1));
+        rootView.addView(wordView2);
+
+        TextView wordView3 = new TextView(this);
+        wordView3.setText(words.get(2));
+        rootView.addView(wordView3);
 
     }
 
