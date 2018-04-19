65e5cd8e910593a75d5cf0ac3ce40da7c783d327
Laszlo1988 <ijulac@yahoo.com>
2018-04-19 11:18:50 +0200

    An ImageView and a placeholder image is added in the list_item.xml file.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -34,7 +34,7 @@ public class FamilyActivity extends AppCompatActivity {
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_listyout file.
+        // word_list.xm layout file.
         ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
