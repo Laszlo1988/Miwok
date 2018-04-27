2e52a8e31abe1a73bfe5396d7d09c88869320a00
Laszlo1988 <ijulac@yahoo.com>
2018-04-27 16:20:31 +0200

    My code is mostly replaced with the Udacity version of code (except for the ArrayList).
    One main mistakes were tackled in every activity:
    
    1. The OnItemClick methods first parameter was wrong as follows.
    
    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id)
    
    Correctly: (Instead of "parent": adapterView)
    
    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
          @Override
          public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)

--- app/src/main/java/com/example/android/miwok/NumbersFragment.java
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -11,21 +11,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 /**
- * A simple {@link Fragment} subclass.
+ * {@link Fragment} that displays a list of number vocabulary words.
  */
 public class NumbersFragment extends Fragment {
 
-    //In this variable, an instance of the MediaPlayer class will be created.
-    private MediaPlayer mMediaPlayer;
     /**
-     * Handles audio focus when playing a sound file
+     * Handles playback of all the sound files
      */
+    private MediaPlayer mMediaPlayer;
+
+    /** Handles audio focus when playing a sound file */
     private AudioManager mAudioManager;
+
     /**
      * This listener gets triggered whenever the audio focus changes
      * (i.e., we gain or lose audio focus because of another app or device).
@@ -33,9 +34,8 @@ public class NumbersFragment extends Fragment {
     private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
         @Override
         public void onAudioFocusChange(int focusChange) {
-            if (focusChange == mAudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                    focusChange == mAudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
                 // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
                 // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
                 // our app is allowed to continue playing sound but at a lower volume. We'll treat
@@ -45,16 +45,13 @@ public class NumbersFragment extends Fragment {
                 // play the word from the beginning when we resume playback.
                 mMediaPlayer.pause();
                 mMediaPlayer.seekTo(0);
-
-            } else if (focusChange == mAudioManager.AUDIOFOCUS_GAIN) {
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
                 // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
                 mMediaPlayer.start();
-
-            } else if (focusChange == mAudioManager.AUDIOFOCUS_LOSS) {
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
                 // The AUDIOFOCUS_LOSS case means we've lost audio focus and
                 // Stop playback and clean up resources
                 releaseMediaPlayer();
-
             }
         }
     };
@@ -66,6 +63,7 @@ public class NumbersFragment extends Fragment {
     private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
             releaseMediaPlayer();
         }
     };
@@ -79,6 +77,7 @@ public class NumbersFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.word_list, container, false);
 
+        // Create and setup the {@link AudioManager} to request audio focus
         mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
 
         final ArrayList<Word> words = new ArrayList<Word>();
@@ -117,11 +116,10 @@ public class NumbersFragment extends Fragment {
         // {@link ListView} will display list items for each {@link Word} in the list.
         listView.setAdapter(adapter);
 
-        //OnItemClickListener is created. Tapping on each Word, the audio file presenting the correct pronunciation is played.
+        // Set a click listener to play the audio when the list item is clicked on
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 // Release the media player if it currently exists because we are about to
                 // play a different sound file
                 releaseMediaPlayer();
@@ -132,14 +130,17 @@ public class NumbersFragment extends Fragment {
                 // Request audio focus so in order to play the audio file. The app needs to play a
                 // short audio file, so we will request audio focus with a short amount of time
                 // with AUDIOFOCUS_GAIN_TRANSIENT.
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC,
-                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
 
                 if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
                     // We have audio focus now.
 
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
                     mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
 
+                    // Start the audio file
                     mMediaPlayer.start();
 
                     // Setup a listener on the media player, so that we can stop and release the
